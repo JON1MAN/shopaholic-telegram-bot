65dc0a20141e6c1987f57cf5cf4284bba3921b9a
aliaksei <aliaksmarchuk@gmail.com>
2024-11-05 20:09:33 +0100

    start of keyboard

--- src/main/java/com/ShopoholicBot/app/dao/model/Bot.java
+++ src/main/java/com/ShopoholicBot/app/dao/model/Bot.java
@@ -1,11 +1,6 @@
-package com.ShopoholicBot.app.model;
+package com.ShopoholicBot.app.dao.model;
 
-import com.ShopoholicBot.app.service.ScraperService;
-import io.github.cdimascio.dotenv.Dotenv;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.ShopoholicBot.app.service.scraper.ScraperService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -13,21 +8,37 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.List;
 
 @Component
 public class Bot extends TelegramLongPollingBot {
 
     private ScraperService scraperService;
-    private Dotenv dotenv;
+    @Value("${TeleShopaholicBot}")
     private String BOT_USERNAME;
+    @Value("${TELEGRAM_BOT_TOKEN}")
     private String TOKEN;
+
+    private InlineKeyboardButton yes = InlineKeyboardButton.builder()
+            .text("Yes")
+            .callbackData("YES")
+            .build();
+
+    private InlineKeyboardButton no = InlineKeyboardButton.builder()
+            .text("No")
+            .callbackData("NO")
+            .build();
+
+    InlineKeyboardMarkup keyboard = InlineKeyboardMarkup.builder()
+            .keyboardRow(List.of(yes, no))
+            .build();
 
     public Bot() {
         this.scraperService = new ScraperService();
-        this.dotenv = Dotenv.load();
-        this.BOT_USERNAME = dotenv.get("TELEGRAM_BOT_USERNAME");
-        this.TOKEN = dotenv.get("TELEGRAM_BOT_TOKEN");
     }
 
     @Override
@@ -51,10 +62,10 @@ public class Bot extends TelegramLongPollingBot {
         System.out.println("Wrote:" + messageText);
         Product product = scraperService.scrapePage(messageText);
 
-        sendMessage(id, product);
+        sendInfoOfProductMessage(id, product);
     }
 
-    public void sendMessage(Long who, Product product){
+    public void sendInfoOfProductMessage(Long who, Product product){
         String caption = String.format(
                 "%s\nPrice: %.2f PLN\nPrice on Sale: %.2f PLN\nIs on Sale: %b",
                 product.getName(),
@@ -70,6 +81,21 @@ public class Bot extends TelegramLongPollingBot {
 
         try{
             execute(sp);
+            sendKeyboard(who, "Do you want to track this Product?", keyboard);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void sendKeyboard(Long who, String text, InlineKeyboardMarkup keyboard) {
+        SendMessage message = SendMessage.builder()
+                .chatId(who.toString())
+                .parseMode("HTML").text(text)
+                .replyMarkup(keyboard)
+                .build();
+
+        try {
+            execute(message);
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);
         }

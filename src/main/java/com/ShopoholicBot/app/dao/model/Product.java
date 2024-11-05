fcc3204ac81dad9407334460613134769799ae3b
aliaksei <aliaksmarchuk@gmail.com>
2024-11-05 20:10:19 +0100

    added product as an entity

--- src/main/java/com/ShopoholicBot/app/dao/model/Product.java
+++ src/main/java/com/ShopoholicBot/app/dao/model/Product.java
@@ -1,5 +1,6 @@
-package com.ShopoholicBot.app.model;
+package com.ShopoholicBot.app.dao.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,10 +8,22 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "product")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private String url;
     private String name;
     private String image;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
     private double price;
     private double priceOnSale;
     private boolean isOnSale;

69018d74e9bc864e33d6983f0ab020f7e1eca634
aliaksei <aliaksmarchuk@gmail.com>
2024-11-09 02:49:28 +0100

    handling pressed buttons

--- src/main/java/com/ShopoholicBot/app/dao/model/Bot.java
+++ src/main/java/com/ShopoholicBot/app/dao/model/Bot.java
@@ -1,11 +1,15 @@
 package com.ShopoholicBot.app.dao.model;
 
 import com.ShopoholicBot.app.service.scraper.ScraperService;
+import jakarta.annotation.PostConstruct;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
@@ -17,28 +21,37 @@ import java.util.List;
 @Component
 public class Bot extends TelegramLongPollingBot {
 
-    private ScraperService scraperService;
-    @Value("${TeleShopaholicBot}")
+    private final ScraperService scraperService;
+
+    @Value("${telegram.TELEGRAM_BOT_USERNAME}")
     private String BOT_USERNAME;
-    @Value("${TELEGRAM_BOT_TOKEN}")
-    private String TOKEN;
+
+    @Value("${telegram.TELEGRAM_BOT_TOKEN}")
+    private String BOT_TOKEN;
 
     private InlineKeyboardButton yes = InlineKeyboardButton.builder()
             .text("Yes")
-            .callbackData("YES")
+            .callbackData("YES_BUTTON")
             .build();
 
     private InlineKeyboardButton no = InlineKeyboardButton.builder()
             .text("No")
-            .callbackData("NO")
+            .callbackData("NO_BUTTON")
             .build();
 
-    InlineKeyboardMarkup keyboard = InlineKeyboardMarkup.builder()
+    private InlineKeyboardMarkup keyboard = InlineKeyboardMarkup.builder()
             .keyboardRow(List.of(yes, no))
             .build();
 
     public Bot() {
+        super();
         this.scraperService = new ScraperService();
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Bot Username: " + getBotUsername());
+        System.out.println("Bot Token: " + getBotToken());
     }
 
     @Override
@@ -48,24 +61,36 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public String getBotToken() {
-        return TOKEN;
+        return BOT_TOKEN;
     }
 
     @Override
     public void onUpdateReceived(Update update) {
-        var user = update.getMessage().getFrom();
-        var message = update.getMessage();
-        var id = user.getId();
+        if (update.getMessage() != null) {
+            var user = update.getMessage().getFrom();
+            var message = update.getMessage();
+            var id = user.getId();
 
-        System.out.println("User: " + user.getFirstName());
-        String messageText = message.getText();
-        System.out.println("Wrote:" + messageText);
-        Product product = scraperService.scrapePage(messageText);
+            System.out.println("User: " + user.getFirstName());
+            String messageText = message.getText();
+            System.out.println("Wrote:" + messageText);
+            Product product = scraperService.scrapePage(messageText);
 
-        sendInfoOfProductMessage(id, product);
+            sendInfoOfProductMessage(id, product);
+        }
+
+        if (update.hasCallbackQuery()) {
+            CallbackQuery callbackQuery = update.getCallbackQuery();
+            String callbackData = callbackQuery.getData();
+
+            if ("YES_BUTTON".equals(callbackData)) {
+                sendCallBackResponse(callbackQuery.getMessage().getChatId(), "YES BUTTON");
+            }
+        }
     }
 
     public void sendInfoOfProductMessage(Long who, Product product){
+        String imageUrl = product.getImage().replace("https", "http");
         String caption = String.format(
                 "%s\nPrice: %.2f PLN\nPrice on Sale: %.2f PLN\nIs on Sale: %b",
                 product.getName(),
@@ -75,7 +100,7 @@ public class Bot extends TelegramLongPollingBot {
         );
         SendPhoto sp = SendPhoto.builder()
                 .chatId(who.toString())
-                .photo(new InputFile(product.getUrl()))
+                .photo(new InputFile(imageUrl))
                 .caption(caption)
                 .build();
 
@@ -96,6 +121,19 @@ public class Bot extends TelegramLongPollingBot {
 
         try {
             execute(message);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void sendCallBackResponse(Long chatId, String text) {
+        SendMessage sendMessage = SendMessage.builder()
+                .text("You choose " + text)
+                .chatId(String.valueOf(chatId))
+                .build();
+
+        try {
+            execute(sendMessage);
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);
         }

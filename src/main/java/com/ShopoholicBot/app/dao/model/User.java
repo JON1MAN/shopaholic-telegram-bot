63311d38454d3469a8eea60e7a4ae2d60d380d6c
aliaksei <aliaksmarchuk@gmail.com>
2024-11-15 00:03:31 +0100

    wishlist:

--- src/main/java/com/ShopoholicBot/app/dao/model/User.java
+++ src/main/java/com/ShopoholicBot/app/dao/model/User.java
@@ -1,11 +1,17 @@
 package com.ShopoholicBot.app.dao.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "user")
 public class User {
@@ -16,4 +22,15 @@ public class User {
     @Column(name = "username", nullable = false, length = 100)
     private String username;
 
+    @ManyToMany
+    @JoinTable(
+            name = "wishlist",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private List<Product> wishList;
+
+    public void addItem(Product product) {
+        wishList.add(product);
+    }
 }

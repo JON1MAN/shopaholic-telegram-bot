c047709ba520877acb10e3535f84eb93ec063d58
aliaksei <aliaksmarchuk@gmail.com>
2024-11-15 00:04:25 +0100

    user and product repo

---
+++ src/main/java/com/ShopoholicBot/app/dao/repository/user/UserRepository.java
@@ -0,0 +1,11 @@
+package com.ShopoholicBot.app.dao.repository.user;
+
+import com.ShopoholicBot.app.dao.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}

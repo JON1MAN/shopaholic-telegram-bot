1784d9aaede7c47d39da6206401d718b38d985d4
aliaksei <aliaksmarchuk@gmail.com>
2024-12-02 18:38:23 +0100

    querry to fetch products from wishlist

--- src/main/java/com/ShopoholicBot/app/dao/repository/product/ProductRepository.java
+++ src/main/java/com/ShopoholicBot/app/dao/repository/product/ProductRepository.java
@@ -2,6 +2,7 @@ package com.ShopoholicBot.app.dao.repository.product;
 
 import com.ShopoholicBot.app.dao.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +12,12 @@ import java.util.Optional;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Optional<Product> findByName(String name);
+
+    @Query(value = """
+    SELECT p
+    FROM User u
+    INNER JOIN u.wishList p
+    """)
+    List<Product> selectAllProductsFromWishlist();
 
 }
